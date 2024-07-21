e9def005821f4dc041944a49bf5ac0d00f7dfca3
Rishu764 <rishabhbhatt218@gmail.com>
2024-07-21 18:13:45 +0530

    add animation

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "../css/loading.css"; 
+import "../css/loading.css";
 
 function LoadingScreen() {
   return (
     <div className="loading-screen fixed inset-0 flex items-center justify-center bg-black z-50">
-      <div className="loading-bar bar1 bg-white"></div>
-      <div className="loading-bar bar2 bg-white"></div>
-      <div className="loading-bar bar3 bg-white"></div>
+      <div className="loading-bar bar1"></div>
+      <div className="loading-bar bar2"></div>
+      <div className="loading-bar bar3"></div>
     </div>
   );
 }
