6e4d2cf109912854469fd25d8c331050e7aa99bc
ayush969696 <ayush972982@gmail.com>
2024-07-27 11:09:16 +0530

    adding

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,16 +3,17 @@ import React, { useEffect } from "react";
 import ContactBanner from "../components/ContactBanner";
 import Contactus from "../components/Contactus";
 import FollowOnSocailM from "../components/FollowOnSocailM";
+import Subscribe from "../components/Subscribe";
 
 function Contact() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-  <div className="pt-24 bg-gray-50">
+  <div className="pt-24">
     <ContactBanner />
     <Contactus />
-    <FollowOnSocailM />
+    <Subscribe />
   </div>
   );
 }
