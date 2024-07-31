ebea3e78e751c542944ac36cec0254990d3055bd
ayush969696 <ayush972982@gmail.com>
2024-07-31 13:12:47 +0530

    adding new things

--- src/Pages/Architects.jsx
+++ src/Pages/Architects.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ArchitectCard from '../components/ArchitectCard';
+import React, { useState } from "react";
+import ArchitectCard from "../components/ArchitectCard";
 
 const architects = [
   {
-    image: "https://via.placeholder.com/400x300",
+    image:
+      "https://images.pexels.com/photos/110469/pexels-photo-110469.jpeg?auto=compress&cs=tinysrgb&w=600",
     name: "John Doe",
     title: "Lead Architect",
-    description: "John is a visionary architect with over 20 years of experience in sustainable design.",
+    description:
+      "John is a visionary architect with over 20 years of experience in sustainable design.",
     rating: 4,
-    address: "123 Main St, Springfield, IL",
+    address: "Dehradun, Uttarakhand",
     phone: "(123) 456-7890",
+    country: "India",
+    state: "Uttarakhand",
+    city: "Dehradun",
     social: [
       { platform: "Facebook", url: "https://facebook.com" },
       { platform: "Twitter", url: "https://twitter.com" },
@@ -17,13 +22,18 @@ const architects = [
     ],
   },
   {
-    image: "https://via.placeholder.com/400x300",
-    name: "John Doe",
-    title: "Lead Architect",
-    description: "John is a visionary architect with over 20 years of experience in sustainable design.",
-    rating: 4,
-    address: "123 Main St, Springfield, IL",
-    phone: "(123) 456-7890",
+    image:
+      "https://images.pexels.com/photos/3333923/pexels-photo-3333923.jpeg?auto=compress&cs=tinysrgb&w=600",
+    name: "Jane Smith",
+    title: "Senior Architect",
+    description:
+      "Jane has a keen eye for detail and has been part of numerous high-profile projects.",
+    rating: 5,
+    address: "Lucknow, Uttarpradesh",
+    phone: "(987) 654-3210",
+    country: "India",
+    state: "Uttarpradesh",
+    city: "Lucknow",
     social: [
       { platform: "Facebook", url: "https://facebook.com" },
       { platform: "Twitter", url: "https://twitter.com" },
@@ -33,10 +43,73 @@ const architects = [
 ];
 
 const Architects = () => {
+  const [searchQueryCountry, setSearchQueryCountry] = useState("");
+  const [searchQueryState, setSearchQueryState] = useState("");
+  const [searchQueryCity, setSearchQueryCity] = useState("");
+
+  const handleSearchChangeCountry = (e) => {
+    setSearchQueryCountry(e.target.value);
+  };
+
+  const handleSearchChangeState = (e) => {
+    setSearchQueryState(e.target.value);
+  };
+
+  const handleSearchChangeCity = (e) => {
+    setSearchQueryCity(e.target.value);
+  };
+
+  const filteredArchitects = architects.filter((architect) => {
+    return (
+      architect.country
+        .toLowerCase()
+        .includes(searchQueryCountry.toLowerCase()) &&
+      architect.state.toLowerCase().includes(searchQueryState.toLowerCase()) &&
+      architect.city.toLowerCase().includes(searchQueryCity.toLowerCase())
+    );
+  });
+
   return (
-    <div className="container mx-auto px-4 py-[10rem] mt-6">
-      <div className="flex items-center gap-2">
-        {architects.map((architect) => (
+    <div className="container lg:px-10 px-2 py-32">
+      <div className="mb-8  lg:p-12 p-4 bg-teal-500 text-white  -skew-y-1 ">
+        <div class="flex flex-col items-center py-8 text-center lg:py-12">
+          <div class="w-full px-4 lg:w-[90%] lg:px-0">
+            <div class="mb-8">
+              <h2 class="text-3xl lg:text-4xl font-bold mb-3">
+                Looking for an Architect?
+              </h2>
+              <p class="text-lg lg:text-xl opacity-80">
+                Search the forum for the answer to your question
+              </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3   gap-4">
+              <input
+                type="text"
+                placeholder="Search by City"
+                value={searchQueryCity}
+                onChange={handleSearchChangeCity}
+                className="w-full rounded-md px-2 py-4 focus:outline-none text-black focus:ring-2 focus:ring-orange-600 focus:border-transparent"
+              />
+              <input
+                type="text"
+                placeholder="Search by State"
+                value={searchQueryState}
+                onChange={handleSearchChangeState}
+                className="w-full rounded-md px-2 py-4 focus:outline-none text-black focus:ring-2 focus:ring-orange-600 focus:border-transparent"
+              />
+              <input
+                type="text"
+                placeholder="Search by Country"
+                value={searchQueryCountry}
+                onChange={handleSearchChangeCountry}
+                className="w-full rounded-md px-2 py-4 focus:outline-none text-black focus:ring-2 focus:ring-orange-600 focus:border-transparent"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {filteredArchitects.map((architect) => (
           <ArchitectCard key={architect.name} {...architect} />
         ))}
       </div>
--- src/components/ArchitectCard.jsx
+++ src/components/ArchitectCard.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaStar, FaRegStar, FaMapMarkerAlt, FaPhoneAlt, FaWhatsapp } from 'react-icons/fa';
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaStar,
+  FaRegStar,
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+  FaWhatsapp,
+} from "react-icons/fa";
 
-const ArchitectCard = ({ image, name, title, description, rating, address, phone, social }) => {
+const ArchitectCard = ({
+  image,
+  name,
+  title,
+  description,
+  rating,
+  address,
+  phone,
+  social,
+}) => {
   const socialIcons = {
     Facebook: <FaFacebookF />,
     Twitter: <FaTwitter />,
@@ -11,15 +29,25 @@ const ArchitectCard = ({ image, name, title, description, rating, address, phone
   const renderRating = (rating) => {
     const stars = [];
     for (let i = 1; i <= 5; i++) {
-      stars.push(i <= rating ? <FaStar key={i} className="text-yellow-500" /> : <FaRegStar key={i} className="text-yellow-500" />);
+      stars.push(
+        i <= rating ? (
+          <FaStar key={i} className="text-yellow-500" />
+        ) : (
+          <FaRegStar key={i} className="text-yellow-500" />
+        )
+      );
     }
     return stars;
   };
 
   return (
-    <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:-translate-y-2 border border-gray-200">
+    <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white hover:shadow-2xl transition-all duration-500 ease-in-out hover:-translate-y-1 border border-gray-200">
       <div className="relative h-48">
-        <img className="w-full h-full object-cover rounded-t-2xl" src={image} alt={`${name}'s image`} />
+        <img
+          className="w-full h-full object-cover rounded-t-xl"
+          src={image}
+          alt={`${name}'s image`}
+        />
         <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-75"></div>
         <div className="absolute bottom-0 left-0 p-4">
           <h3 className="text-white text-lg font-bold">{name}</h3>
@@ -27,25 +55,35 @@ const ArchitectCard = ({ image, name, title, description, rating, address, phone
         </div>
       </div>
       <div className="px-6 py-4">
-        <div className="flex items-center mb-2">
-          {renderRating(rating)}
-        </div>
+        <div className="flex items-center mb-2">{renderRating(rating)}</div>
         <p className="text-gray-700 text-base mt-2">{description}</p>
         <div className="flex items-center text-gray-500 text-sm mt-2">
           <FaMapMarkerAlt className="mr-2" />
           {address}
         </div>
-        <div className="flex items-center text-gray-500 text-sm mt-2">
-          <FaPhoneAlt className="mr-2" />
-          {phone}
+
+        <div className="flex">
+          <div className="px-2 py-4 flex justify-center">
+            <button className="flex items-center text-gray-500 text-sm mt-2 border-2 border-green-500 py-2 hover:bg-green-500 hover:text-white rounded-full px-6 transition-colors duration-300 ">
+              <FaPhoneAlt className="mr-2" />
+              {phone}
+            </button>
+          </div>
+
+          <div className="px-2 py-4 flex justify-center">
+            <button className="bg-green-500 text-white rounded-full px-4 py-2 text-sm font-semibold hover:bg-green-600 transition-colors duration-300 flex items-center">
+              <FaWhatsapp className="mr-2" size={20} />
+              Chat Now
+            </button>
+          </div>
         </div>
       </div>
-      <div className="px-6 pt-4 pb-2 flex flex-wrap">
+      <div className="px-6 pb-3 flex flex-wrap">
         {social.map((link, index) => (
           <a
             key={index}
             href={link.url}
-            className="inline-block bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 hover:from-blue-600 hover:to-indigo-600 transition-colors duration-300 flex items-center"
+            className=" bg-gradient-to-r from-red-500 to-orange-500 text-white rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 hover:from-red-600 hover:to-orange-600 transition-all duration-300 ease-in-out flex items-center"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -53,12 +91,6 @@ const ArchitectCard = ({ image, name, title, description, rating, address, phone
             <span className="ml-2">{link.platform}</span>
           </a>
         ))}
-      </div>
-      <div className="px-6 py-4 flex justify-center">
-        <button className="bg-green-500 text-white rounded-full px-4 py-2 text-sm font-semibold hover:bg-green-600 transition-colors duration-300 flex items-center">
-          <FaWhatsapp className="mr-2" />
-          Chat Now
-        </button>
       </div>
     </div>
   );
